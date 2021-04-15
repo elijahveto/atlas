e4c24cd604171e26d4d81c240c07a943ece1b370
elijah <67555816+elijahveto@users.noreply.github.com>
2021-04-15 10:52:17 +0200

    jsdjk<jsdsj

--- src/novalis/novalis/settings.py
+++ src/novalis/novalis/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jhfdjhfjdhfjdfjsdhfjkllk1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'lit-waters-05102.herokuapp.com']
 
