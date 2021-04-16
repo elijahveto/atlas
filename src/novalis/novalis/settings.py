e762e0d67f5f0268e43690a78119b6801eda2d0f
elijahveto <67555816+elijahveto@users.noreply.github.com>
2021-04-16 09:47:10 +0200

    Update settings.py

--- src/novalis/novalis/settings.py
+++ src/novalis/novalis/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jhfdjhfjdhfjdfjsdhfjkllk1'
+SECRET_KEY = os.environ.get('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
